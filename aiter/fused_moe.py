c52adaa47948b683b8da51f18ce47336e8989328
Fabian Wahlster <Fabian.Wahlster@amd.com>
2025-10-13 07:04:36 +0000

    QuantType.per_1x128 allow inter_dim % 128 for fused_moe_1stage

--- aiter/fused_moe.py
+++ aiter/fused_moe.py
@@ -574,7 +574,7 @@ def get_2stage_cfgs(
             in fused_moe_1stage_dict[get_gfx()]
         ):
             if q_type == QuantType.per_1x128:
-                run_1stage = True and (inter_dim % 256 == 0)
+                run_1stage = True and (inter_dim % 128 == 0)
             elif q_type == QuantType.per_Token and q_dtype_w in [dtypes.i8, dtypes.fp8]:
                 run_1stage = token > 32
             elif q_type != QuantType.per_1x32:
